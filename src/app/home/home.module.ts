0183b4977a23693bd3f6e4479de36f5f1c46e1cf
mostafa hamed <mos.hamed11@hotmail.com>
2019-02-03 16:02:28 +0200

    init

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 
+
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
+      CommonModule,
+      ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild([
       {
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+    declarations: [HomePage],
+
 })
 export class HomePageModule {}
